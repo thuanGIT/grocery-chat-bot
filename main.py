1870a8f1489b862ab41e7c07fddc77a99c0b3ba8
Thuan Vo <thuan.votann@gmail.com>
2022-04-07 17:18:44 -0700

    Set up Flask App

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from app.agent import Agent
-# from dialogflow_migration.bot import Bot
+from flask import Flask, jsonify
+from datetime import datetime
+from app.utilities.logs import Log
 
-def main():
-    agent = Agent()
-    #initiate conversation with customer
-    agent.start_conversation()
+# Create a flask app
+app = Flask(__name__)
+app.config["TAG"] = "Flask App"
+app.config["SESSIONS"] = set()
+app.config["AGENT"] = Agent()
 
+@app.route("/")
+def init_conversation():
+  # Create a unique identifier
+  session_id = str(datetime.now()).encode("utf-8").hex()
+  app.config["SESSIONS"].add(session_id)
 
-if __name__ == '__main__':
-  main()
+  # Logging
+  Log.d(app.config["TAG"], f"Conversation id#{session_id} is initialized")
+
+  return jsonify({"session_id": session_id, "status":"OK"})
+
+@app.route("/<session_id>")
+def receive_message(session_id):
+  # TODO: Get the agent to process message
+  ...
