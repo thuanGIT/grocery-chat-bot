825c4b664925a534070c9eeb88f1fe36dfa776eb
Thuan Vo <thuan.votann@gmail.com>
2022-04-10 05:12:58 -0700

    Init database connection in base handler

--- app/base_handler.py
+++ app/base_handler.py
@@ -1,12 +1,15 @@
 from abc import abstractmethod
 import os
+from app.database import Database
 class BaseHandler:
     """Base handler interface for all intent handler.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, session_id):
         # Set the operation mode (i.e. DEV or PRODUCTION)
         self.runtime_mode = os.getenv("PYTHON_ENV", "DEV")
+        self.db = Database.instance(session_id=session_id)
+        self.db.connect()
 
     @abstractmethod
     def handle(self, **kwargs) -> str: ...
