b2d28c02639bb005851d7469da98b43a64ffc8b5
Thuan Vo <thuan.votann@gmail.com>
2022-04-10 04:42:55 -0700

    Fix schema for store & Add command to copy data from csv

--- data/ddl.sql
+++ data/ddl.sql
@@ -4,20 +4,18 @@ DDL for database tables using PostgreSQL.
 
 CREATE TABLE Store (
    StoreId  SERIAL,
-   Name VARCHAR(50)            NOT NULL,
    Address VARCHAR(50)         NOT NULL,
-   PhoneNumber VARCHAR(10)     NOT NULL,
+   PhoneNumber VARCHAR(15)     NOT NULL,
    Website VARCHAR(50),
    OpeningHours VARCHAR(20)    NOT NULL,
-   PriceRange VARCHAR(10),
    PRIMARY KEY (StoreId)
 );
 
 CREATE TABLE Product (
    ProductId     CHAR(5)       NOT NULL,
    ProductName   VARCHAR(50),
-   Category      VARCHAR(50),
    ListPrice     DECIMAL(9, 2),
+   Category      VARCHAR(50),
    InStock       BOOLEAN       NOT NULL,
    PRIMARY KEY (ProductId)
 );
@@ -67,3 +65,9 @@ CREATE TABLE OrderedProduct (
    FOREIGN KEY (ProductId) REFERENCES Product (ProductId) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
+COPY Customer FROM '/docker-entrypoint-initdb.d/samples/customer.csv' DELIMITER ',' CSV HEADER;
+COPY Employee FROM '/docker-entrypoint-initdb.d/samples/employee.csv' DELIMITER ',' CSV HEADER;
+COPY Orders FROM '/docker-entrypoint-initdb.d/samples/orders.csv' DELIMITER ',' CSV HEADER;
+COPY Product FROM '/docker-entrypoint-initdb.d/samples/product.csv' DELIMITER ',' CSV HEADER;
+COPY Store(Address, PhoneNumber, Website, OpeningHours) FROM '/docker-entrypoint-initdb.d/samples/store.csv' DELIMITER ',' CSV HEADER;
+COPY OrderedProduct FROM '/docker-entrypoint-initdb.d/samples/orderedproduct.csv' DELIMITER ',' CSV HEADER;
